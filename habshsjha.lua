6ff39b4da32a37eecb482174bf0e9d8efbd9a489
zuqorl <alextheo482@gmail.com>
2025-03-01 20:19:41 +0100

    Create habshsjha.lua

---
+++ habshsjha.lua
@@ -0,0 +1,2 @@
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/Test1/refs/heads/main/sursa.lua.lua"))():MakePrototypeLibrary("CDTStorm")
+local CuteTab = Lib:MakeTab("my little adorable tab", true) -- true to make it open by default (optional)
