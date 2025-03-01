ad1362f70fb2b1da418906a6276dd8e742606d3d
zuqorl <alextheo482@gmail.com>
2025-03-01 20:25:43 +0100

    Update habshsjha.lua

--- habshsjha.lua
+++ habshsjha.lua
@@ -1,5 +1,8 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/Test1/refs/heads/main/sursa.lua.lua"))():MakePrototypeLibrary("CDTStorm")
 local CuteTab = Lib:MakeTab("Test n*1", true) -- true to make it open by default (optional)
+
+CuteTab:Info("i love cute cats")
+
 CuteTab:Button("Do you like CDTStorm?", function()
 print("I do")
 end)
