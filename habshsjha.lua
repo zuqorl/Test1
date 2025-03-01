20d2b0577f4097e3a4bd3408b5c75b1b17e1d026
zuqorl <alextheo482@gmail.com>
2025-03-02 00:06:43 +0100

    Update habshsjha.lua

--- habshsjha.lua
+++ habshsjha.lua
@@ -1,12 +1,12 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/Test1/refs/heads/main/sursa.lua.lua"))():MakePrototypeLibrary("CDTStorm")
-local CuteTab = Lib:MakeTab("Money", true) -- true to make it open by default (optional)
+local CuteTab = Lib:MakeTab("Information", true) -- true to make it open by default (optional)
 
-CuteTab:Info("Farm money option")
+CuteTab:Info("Made by zuqo")
 
 CuteTab:Button("Do you like CDTStorm?", function()
 print("I do")
 end)
-CuteTab:Toggle("nigga", function(value)
+CuteTab:Toggle("Test", function(value)
 print(value)
 end)
 local CuteTab = Lib:MakeTab("Race", true) -- true to make it open by default (optional)
