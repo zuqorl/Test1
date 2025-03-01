b29c26614fd3e32ddead29b9b3105fcce76fa64e
zuqorl <alextheo482@gmail.com>
2025-03-02 00:48:48 +0100

    Update habshsjha.lua

--- habshsjha.lua
+++ habshsjha.lua
@@ -1,1 +1,3 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/Test1/refs/heads/main/sursa.lua.lua"))():MakePrototypeLibrary("CDTStorm")
+-First rep
+local CuteTab = Lib:MakeTab("Information", true) -- true to make it open by default (optional)
